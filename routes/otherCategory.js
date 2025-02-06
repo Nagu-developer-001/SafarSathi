bb214fa0186e37b0d53477e316bd9cadbd105505
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2025-02-06 09:31:50 +0530

    added files

--- routes/otherCategory.js
+++ routes/otherCategory.js
@@ -4,5 +4,8 @@ const {UniqueUrl} = require("../AuthenticLogin.js");
 let otherCategoryController = require("../controllers/otherCategory.js");
 router.get("/listings/FAQ",UniqueUrl,otherCategoryController.renderFAQ);
 router.get("/listings/help",UniqueUrl,otherCategoryController.renderHelp);
+router.get("/listings/search",(req,res)=>{
+    console.log(req.query);
+});
 
 module.exports = router;
